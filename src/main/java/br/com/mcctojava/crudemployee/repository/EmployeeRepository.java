610faefe1ffa7e8dddcf4f7ba500985306b9169f
Lukas Veiga <lukas.veiga@hotmail.com>
2023-04-20 12:37:30 -0300

    updates

--- src/main/java/br/com/mcctojava/crudemployee/repository/EmployeeRepository.java
+++ src/main/java/br/com/mcctojava/crudemployee/repository/EmployeeRepository.java
@@ -4,7 +4,6 @@ import br.com.mcctojava.crudemployee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     List<Employee> findByFirstName(String FirstName);
